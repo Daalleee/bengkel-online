27e3a26b3020a5e5a75173216a386ce4fca22c8d
Daalleee <dalemasan10@gmail.com>
2025-06-10 07:52:09 +0700

    model review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -12,4 +13,14 @@ class Review extends Model
         'rating',
         'comment',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bengkel(): BelongsTo
+    {
+        return $this->belongsTo(Bengkel::class);
+    }
 }
