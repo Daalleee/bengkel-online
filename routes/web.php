bcc86e50c39d8dbdfa813e9287247f0a90d946bd
Daalleee <dalemasan10@gmail.com>
2025-06-10 09:25:28 +0700

    buat routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BengkelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('bengkels', BengkelController::class);
