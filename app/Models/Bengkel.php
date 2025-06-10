0ff02ecaa3fa638a2062c2ae32787d0df61a688a
Daalleee <dalemasan10@gmail.com>
2025-06-10 07:48:08 +0700

    model bengkel

--- app/Models/Bengkel.php
+++ app/Models/Bengkel.php
@@ -3,18 +3,42 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bengkel extends Model
 {
+    protected $table = 'bengkels'; // pastikan sesuai nama tabel di migration
+
     protected $fillable = [
         'user_id',
         'name',
         'address',
-        'latidude',
-        'longtude',
+        'latitude',
+        'longitude',
         'description',
         'opening_time',
         'closing_time',
-
     ];
+
+    protected $casts = [
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'opening_time' => 'datetime:H:i:s',
+        'closing_time' => 'datetime:H:i:s',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function services(): HasMany
+    {
+        return $this->hasMany(Service::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
 }
