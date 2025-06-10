20ca9f093b2403f05f208eab4a806838abef0e2b
Daalleee <dalemasan10@gmail.com>
2025-06-10 07:51:16 +0700

    model chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -3,12 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chat extends Model
 {
     protected $fillable = [
         'sender_id',
-        'receiver_Id',
-        'mesagge',
+        'receiver_id',
+        'message',
     ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
